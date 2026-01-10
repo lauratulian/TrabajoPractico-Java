294bdb6717df26c15a509fae000340620e6a7a11
Laura <lau.tulian01@gmail.com>
2026-01-10 13:48:10 -0300

    Correcion de conexion a la base de datos

--- src/data/DbConnector.java
+++ src/data/DbConnector.java
@@ -1,22 +1,48 @@
 package data;
 
 import java.sql.*;
+import java.util.Properties;
+import java.io.InputStream;
 
 
 public class DbConnector {
 
 	private static DbConnector instancia;
 	
-	private String driver="com.mysql.cj.jdbc.Driver";
-	private String host="localhost";
-	private String port="3306";
-	private String user="java";
-	private String password="himitsu";
-	private String db="libreriatulian";
+	private String driver;
+    private String host;
+    private String port;
+    private String user;
+    private String password;
+    private String db;
+	
 	private int conectados=0;
 	private Connection conn=null;
 	
+	 private void loadConfig() {
+	        try (InputStream is = getClass()
+	                .getClassLoader()
+	                .getResourceAsStream("db.properties")) {
+
+	            Properties props = new Properties();
+	            props.load(is);
+
+	            driver = props.getProperty("db.driver");
+	            host = props.getProperty("db.host");
+	            port = props.getProperty("db.port");
+	            db = props.getProperty("db.name");
+	            user = props.getProperty("db.user");
+	            password = props.getProperty("db.password");
+
+	        } catch (Exception e) {
+	            throw new RuntimeException("No se pudo cargar db.properties", e);
+	        }
+	    }
+
+	
+	
 	public DbConnector() {
+		loadConfig();
 		try {
 			Class.forName(driver);
 		} catch (ClassNotFoundException e) {
@@ -47,7 +73,7 @@ public class DbConnector {
 	public void releaseConn() {
 		conectados--;
 		try {
-			if (conectados<=0) {
+			if (conectados <= 0 && conn != null) {
 				conn.close();
 			}
 		} catch (SQLException e) {
